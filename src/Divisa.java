64e06cd8dd0ed92d7f544de5f2103c7719589d05
Sebastian Betancur Carvalho <sebastianb.0211@gmail.com>
2025-05-07 12:34:57 -0500

    se arregla un bug

--- src/Divisa.java
+++ src/Divisa.java
@@ -1,6 +1,3 @@
 public record Divisa(String base_code, String target_code, double conversion_result) {
 
-    public String toString(double cantidad) {
-        return "Hiciste una conversion de: "+cantidad +" "+base_code+" a "+ conversion_result+ " "+ target_code;
-    }
 }
